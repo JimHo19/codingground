8de5907f46818a0c390f33fdfa110de3a00bd75d
JimHo19 <iamjimho@gmail.com>
2017-07-27 22:03:43 +0800

    Created by Coding Ground Client

---
+++ New Project-20170727/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{   
+    int a=0;
+    char input2[100];
+    printf("what is your  favorite number?");
+    scanf("%int",&a);
+    printf("why");
+    scanf("%s",input2);
+    printf("OK!");
+    
+    
+
+    return 0;
+}
+
